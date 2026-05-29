6c9a5077c59e44b8ece15cdba94f117ff731fa52
Harry <thureinlwin2023@gmail.com>
2026-05-29 03:18:41 -0700

    Declare material icons

--- Interface/expo-vector-icons.d.ts
+++ Interface/expo-vector-icons.d.ts
@@ -13,3 +13,19 @@ declare module '@expo/vector-icons/FontAwesome' {
 
   export default FontAwesome
 }
+
+declare module '@expo/vector-icons/MaterialIcons' {
+  import type { ComponentType } from 'react'
+  import type { TextStyle } from 'react-native'
+
+  type MaterialIconsProps = {
+    name: string
+    size?: number
+    color?: string
+    style?: TextStyle
+  }
+
+  const MaterialIcons: ComponentType<MaterialIconsProps>
+
+  export default MaterialIcons
+}
