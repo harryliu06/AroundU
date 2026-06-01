6d6ec5fd823fbb98236492ff9ad8618c5d4dcac7
Harry <thureinlwin2023@gmail.com>
2026-06-01 16:36:25 -0700

    Implement Integration tests on auth, login and signup

---
+++ Backend/app.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import cors from 'cors'
+
+import {
+  getCurrentUser,
+  getUser,
+  login,
+  signup,
+  nearbyUsers,
+  updateCurrentUser,
+} from './controllers/userController.js'
+import {
+  acceptFriendRequest,
+  addFriend,
+  blockUser,
+  getFriendStatus,
+  listBlockedUsers,
+  listFriendRequests,
+  listFriends,
+  listMessages,
+  sendMessage,
+  unblockUser,
+} from './controllers/socialController.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running!' })
+})
+
+app.post('/signup', signup)
+app.post('/login', login)
+app.get('/me', getCurrentUser)
+app.patch('/me', updateCurrentUser)
+app.get('/users/:id', getUser)
+app.get('/nearby-users', nearbyUsers)
+app.get('/friends', listFriends)
+app.post('/friends/:friendId', addFriend)
+app.get('/friends/:friendId/status', getFriendStatus)
+app.get('/blocked-users', listBlockedUsers)
+app.post('/blocked-users/:userId', blockUser)
+app.delete('/blocked-users/:userId', unblockUser)
+app.get('/friend-requests', listFriendRequests)
+app.post('/friend-requests/:requestId/accept', acceptFriendRequest)
+app.get('/chats/:friendId/messages', listMessages)
+app.post('/chats/:friendId/messages', sendMessage)
+
+export default app
