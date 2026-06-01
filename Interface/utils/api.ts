c41e539de5cdaa1414a539910e32d16ab6686e5d
Harry <thureinlwin2023@gmail.com>
2026-05-31 20:30:11 -0700

    API routing ignore headers

--- Interface/utils/api.ts
+++ Interface/utils/api.ts
@@ -8,6 +8,13 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL || ''
 
 export function apiFetch(path: string, options?: RequestInit) {
   const normalizedPath = path.startsWith('/') ? path : `/${path}`
+  const headers = {
+    'ngrok-skip-browser-warning': 'true',
+    ...(options?.headers || {}),
+  }
 
-  return fetch(`${API_URL}${normalizedPath}`, options)
+  return fetch(`${API_URL}${normalizedPath}`, {
+    ...options,
+    headers,
+  })
 }
